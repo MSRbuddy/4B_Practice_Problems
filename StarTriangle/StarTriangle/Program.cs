4cda6ef3b41caf0d736eca76eff73f0cbb73426b
MSRbuddy <meghanadasari222@gmail.com>
2023-04-13 15:02:18 +0530

    Added: Print stars in Triangle pattern!

---
+++ StarTriangle/StarTriangle/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace StarTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----Print Stars in Triangle pattern!-----\n");
+            int i, j;
+            for(i = 1; i <= 8; i++)
+            {
+                for (j = i; j <= 8; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
