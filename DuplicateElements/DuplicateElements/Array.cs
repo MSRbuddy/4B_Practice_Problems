c2f6d9c2caeb815539ac57fa160adf8ca8ab0af8
MSRbuddy <meghanadasari222@gmail.com>
2023-04-13 14:48:28 +0530

    Added: Count of Duplicate elements in an Array!

---
+++ DuplicateElements/DuplicateElements/Array.cs
@@ -0,0 +1,35 @@
+﻿namespace DuplicateElements
+{
+    internal class Array
+    {
+        public void ArrayInfo()
+        {
+            int[] arr=new int[10];
+            int i, j, count = 0;
+            Console.WriteLine("Elements");
+            for(i=0; i<arr.Length; i++)
+            {
+                Console.WriteLine("Enter Array Element");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Elememnts in Array: ");
+            for(i=0;i<arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            //To find duplicate elements
+            for(i=0;i<arr.Length;i++)
+            {
+                for(j=i+1;j<arr.Length;j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("The no.of duplicate elements in Array : {0}", count);
+        }
+    }
+}
