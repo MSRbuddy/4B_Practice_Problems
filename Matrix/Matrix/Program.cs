0c17dde7964b332e82cb083344caac7b9a0f602d
MSRbuddy <meghanadasari222@gmail.com>
2023-04-13 15:08:43 +0530

    Added: Sum of each Row of a given Matrix!

---
+++ Matrix/Matrix/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Matrix
+{
+    internal class Program
+    {
+        public void SumOfMatrix()
+        {
+            int sum = 0, row, col;
+            Console.WriteLine("How many rows you want in matrix: ");
+            row = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many columns you want in matrix: ");
+            col = Convert.ToInt32(Console.ReadLine());
+            int[,] matrix=new int[row, col];
+
+            for(int i=0;i<row;i++)
+            {
+                for (int j=0;j<col;j++)
+                {
+                    Console.WriteLine("Enter elements of matrix: ");
+                    matrix[i,j]= Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Matrix : ");
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            
+            for (int i = 0; i < row; i++)
+            {
+                sum = 0;
+                for (int j = 0; j < col; j++)
+                {
+                    sum = sum + matrix[i, j];
+                }
+                Console.WriteLine("Sum of row[{0}] : {1}", (i + 1), sum);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.SumOfMatrix();
+        }
+    }
+}
